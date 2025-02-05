15fd79cb149e7310d1c8e6aca1216681ed62892f
Allie Costner <alliecostner3@gmail.com>
2025-02-05 14:22:40 -0500

    Exercise 2 - complete

---
+++ CostnerAllieExercise2/app.js
@@ -0,0 +1,40 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const port = 3000;
+
+const host = "localhost";
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+
+    if (req.url === '/') {
+        path = path + 'index.html';
+    } else if (req.url === '/about'){
+        path = path + 'about.html';
+    } else if (req.url === '/contact'){
+        path = path + 'contact.html';
+    } else {
+        res.statusCode = 404;
+        path = path + '404.html';
+    };
+
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+
+});
+
+server.listen(port, host, () => {
+  console.log('Server is running on port', port);
+});
