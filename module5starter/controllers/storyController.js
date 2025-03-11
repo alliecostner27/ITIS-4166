0461f04384dafc238e01807e8f8f436683e70999
Allie Costner <alliecostner3@gmail.com>
2025-03-11 10:01:18 -0400

    MongoDB integration

--- module5starter/controllers/storyController.js
+++ module5starter/controllers/storyController.js
@@ -10,11 +10,13 @@ exports.new = (req, res)=>{
     res.render('./story/new');
 };
 
-exports.create = (req, res)=>{
+exports.create = (req, res, next)=>{
     //res.send('Created a new story');
     let story = req.body;
-    model.save(story);
-    res.redirect('/stories');
+    story.createdAt = new Date();
+    model.save(story)
+    .then(result => res.redirect('/stories'))
+    .catch(err => next(err));
 };
 
 exports.show = (req, res, next)=>{
@@ -35,36 +37,49 @@ exports.show = (req, res, next)=>{
 
 exports.edit = (req, res, next)=>{
     let id = req.params.id;
-    let story = model.findById(id);
-    if(story) {
+    model.findById(id)
+    .then(story => 
+        {if(story) {
         res.render('./story/edit', {story});
-    } else {
+        } else {
         let err = new Error('Cannot find a story with id ' + id);
         err.status = 404;
         next(err);
-    }
+    }})
+    .catch(err => next(err));
+
 };
 
 exports.update = (req, res, next)=>{
     let story = req.body;
     let id = req.params.id;
 
-   if (model.updateById(id, story)) {
-       res.redirect('/stories/'+id);
-   } else {
-    let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
-        next(err);
-   }
+    model.updateById(id, story)
+    .then(result => {
+        if(result.modifiedCount === 1){
+            res.redirect('/stories/'+id)
+        }else{
+            let err = new Error('Cannot find a story with id ' + id);
+            err.status = 404;
+            next(err);
+        };
+        })
+    .catch(err => next(err));
+
 };
 
 exports.delete = (req, res, next)=>{
     let id = req.params.id;
-    if(model.deleteById(id))
-        res.redirect('/stories');
-    else {
-        let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
-        next(err);
-    }
-};
+
+    model.deleteById(id)
+    .then(result => {
+        if(result.deletedCount === 1){
+            res.redirect('/stories');
+        }else{
+            let err = new Error('Cannot find a story with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err => next(err));
+}
--- module5starter/models/story.js
+++ module5starter/models/story.js
@@ -13,30 +13,8 @@ exports.find = () => stories.find().toArray();
 
 exports.findById = id => stories.findOne({_id: new ObjectId(`${id}`)});
 
-exports.save = function (story) {
-    story.id = uuidv4();
-    story.createdAt = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
-    stories.push(story);
-};
+exports.save = story => stories.insertOne(story);
 
-exports.updateById = function(id, newStory) {
-    let story = stories.find(story=>story.id === id);
-    if(story) {
-        story.title = newStory.title;
-        story.content = newStory.content;
-        return true;
-    } else {
-        return false;
-    }
- 
-}
+exports.updateById =(id, newStory) => stories.updateOne({_id: new ObjectId(`${id}`)}, {$set: {title: newStory.title, content: newStory.content}});
 
-exports.deleteById = function(id) {
-    let index = stories.findIndex(story =>story.id === id);
-    if(index !== -1) {
-        stories.splice(index, 1);
-        return true;
-    } else {
-        return false;
-    }
-}
+exports.deleteById = id => stories.deleteOne()
